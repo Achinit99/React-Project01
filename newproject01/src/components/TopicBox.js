2df79e6498a27eb873b7d780f4b5bc197e01be44
Achinit99 <achinit720@gmail.com>
2023-01-02 17:43:40 +0530

    Link CSS file in react

---
+++ newproject01/src/components/TopicBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './topicbox.css';
+
+export default function TopicBox() {
+  return (
+    <div className="topic">
+      <span>This is the topicbox</span>
+    </div>
+  );
+} 
