c574123c8e65559dbd26f62e2d50115aede1d4c7
Achinit99 <achinit720@gmail.com>
2023-02-25 22:38:14 +0530

    add page
    add page and topicBox

--- newproject01/src/App.js
+++ newproject01/src/App.js
@@ -1,31 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contac from './Pages/Contac';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <switch>
-
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route path="/About">
-            <About />
-          </Route>
-
-          <Route path="/Contac">
-            <Contac />
-          </Route>
-
-        </switch>
-      </div>
-    </Router>
+    <div>
+      <h1>Hi Achini</h1>
+    </div>
   );
 }
 
--- newproject01/src/Pages/Contac.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Contac() {
-  return (
-    <div>
-      <h1>Contac Page</h1>
-    </div>
-  );
-}
