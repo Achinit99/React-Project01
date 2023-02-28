3febaf3775dad6e15f8f6db3c1f7650be5357a1a
Achinit99 <achinit720@gmail.com>
2023-03-01 00:01:10 +0530

    Edit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Pages/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import About from './Pages/About';
-import Contact from './Pages/Contact';
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Home from "./Pages/Home";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import About from "./Pages/About";
+// import Contact from "./Pages/Contact";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+
+//           <Route path="Pages/About">
+//             <About />
+//           </Route>
+
+//           <Route path="/Contact">
+//             <Contact />
+//           </Route>
+//         </switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-        <switch>
-
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route path="/About">
-            <About />
-          </Route>
-
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-
-        </switch>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
       </div>
     </Router>
   );
