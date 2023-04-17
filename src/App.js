4426467ff040d605cb08b7d91508e4e2e6c8a9dd
Achinit99 <achinit720@gmail.com>
2023-04-17 22:36:12 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-// import Home from "./Pages/Home";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import About from "./Pages/About";
-// import Contact from "./Pages/Contact";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <switch>
-//           <Route exact path="/">
-//             <Home />
-//           </Route>
-
-//           <Route path="Pages/About">
-//             <About />
-//           </Route>
-
-//           <Route path="/Contact">
-//             <Contact />
-//           </Route>
-//         </switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./HomePage";
-import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
+        <switch></switch>
       </div>
     </Router>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,12 @@
-import React from 'react'
+import React from "react";
 // import TopicBox from '../components/TopicBox'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <link to="/About"> Go to the about page</link>
-            {/* <TopicBox food="koththu" price="Rs 50" />
-            <TopicBox food="rice" price="Rs 150" />
-            <TopicBox food="buriyani" price="Rs 550" /> */}
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <link to="/About"> Go to the about page</link>
+    </div>
+  );
 }
